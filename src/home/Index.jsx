f26a625d2c9eab4170058a75579da1cf64682d20
FrancisRSweb <68391029+FrancisRSweb@users.noreply.github.com>
2021-10-08 00:14:08 +0200

    Update Index.jsx

--- src/home/Index.jsx
+++ src/home/Index.jsx
@@ -5,7 +5,7 @@ function Home() {
     return (
         <div>
             <h1>React - CRUD con React Hook para HelloAuto</h1>
-            <p>Creacion de un Crud basico para la gestion de Usuarios, estos tiene datos tipo: email. password. Rol.</p>
+            <p>Creacion de un Crud basico para la gestion de Usuarios, estos tiene datos tipo: email. password. Fecha creación.</p>
             <p><Link to="users">&gt;&gt; Administración de Usuarios</Link></p>
         </div>
     );
